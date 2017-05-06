00040347d71f7c520702d860755e9d2ad23ee4ac
AlaaSabah <alaa.m.sabbah@gmail.com>
2017-05-06 13:46:33 -0300

    Min function

--- src/statisticsmeasurements/MinCalculator.java
+++ src/statisticsmeasurements/MinCalculator.java
@@ -1,5 +1,40 @@
 package statisticsmeasurements;
 
-public class MinCalculator {
+import java.util.ArrayList;
 
+public class MinCalculator {
+	
+	public static double findMin(ArrayList<Double> values){
+		
+		if(values.size() == 0)
+			return -1000.0;
+		if(values.size() == 1)
+			return values.get(0).doubleValue();
+		
+		int size = values.size();
+		double min = values.get(0).doubleValue();
+		
+		for(int i = 1; i<size; i++){
+			if(values.get(i).doubleValue() < min){
+				min = values.get(i).doubleValue();
+			}
+		}
+		
+		return min;
+	}
+	
+
+	public static void main(String[] args){
+		
+		ArrayList<Double> v = new ArrayList<Double>();
+		double d = 0.0;
+		System.out.println("The random values are :");
+		for(int i = 0; i<100; i++){
+			d = Math.random()*100.0;
+			v.add(d);
+			System.out.print(d+",  ");
+		}
+		System.out.println();
+		System.out.println("Min value is : "+ MinCalculator.findMin(v));
+	}
 }
