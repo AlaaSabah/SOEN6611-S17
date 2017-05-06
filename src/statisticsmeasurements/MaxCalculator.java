1cf6b982f1671a9e2c6c8456352c7a1b74b713a5
AlaaSabah <alaa.m.sabbah@gmail.com>
2017-05-06 13:40:19 -0300

    Max function

--- src/statisticsmeasurements/MaxCalculator.java
+++ src/statisticsmeasurements/MaxCalculator.java
@@ -1,5 +1,40 @@
 package statisticsmeasurements;
 
+import java.util.ArrayList;
+
 public class MaxCalculator {
+	
+	public static double findMax(ArrayList<Double> values){
+		
+		if(values.size() == 0)
+			return -1000.0;
+		if(values.size() == 1)
+			return values.get(0).doubleValue();
+		
+		int size = values.size();
+		double max = values.get(0).doubleValue();
+		
+		for(int i = 1; i<size; i++){
+			if(values.get(i).doubleValue() > max){
+				max = values.get(i).doubleValue();
+			}
+		}
+		
+		return max;
+	}
+	
+	public static void main(String[] args){
+		
+		ArrayList<Double> v = new ArrayList<Double>();
+		double d = 0.0;
+		System.out.println("The random values are :");
+		for(int i = 0; i<100; i++){
+			d = Math.random()*100.0;
+			v.add(d);
+			System.out.print(d+",  ");
+		}
+		System.out.println();
+		System.out.println("Max value is : "+ MaxCalculator.findMax(v));
+	}
 
 }
